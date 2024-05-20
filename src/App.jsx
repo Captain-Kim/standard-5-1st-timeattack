4ea35a958c70317339d7aa10f676a226e43d2187
Captain-Kim <brighthero@kakao.com>
2024-05-20 15:52:52 +0900

    미완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-export default function App() {
-  return (
+// import Input from "./components/Input";
+import { useState } from "react";
+const [number, setNumber] = useState(0);
+const [inputNumber, setInputNumber] = useState("");
+
+const handleAdd = () => {
+  setNumber(number + number(inputNumber));
+};
+
+const handleSubtract = () => {
+  setNumber(number - number(inputNumber));
+};
+
+const handleInputChange = (e) => {
+  setInputNumber(e.target.value);
+};
+
+return (
+  <div>
+    <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
     <div>
-      <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
-      <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+      <form onSubmit={addNumber}>
+        <input
+          type="number"
+          value={inputValue}
+          onChange={handleInputChange}
+        ></input>{" "}
+        만큼을 <button onClick={handleAdd}>더할게요</button>{" "}
+        <button onClick={handleSubtract}>뺄게요</button>
         <button>초기화</button>
-      </div>
-      <hr />
-      <div>
-        <h3>결과</h3>
-        <p>0</p>
-      </div>
+      </form>
     </div>
-  );
-}
+    <hr />
+    <div>
+      <h3>결과</h3>
+      <p>0</p>
+    </div>
+  </div>
+);
